11cd5b099c5a876b2ad0f54088bc09425ff04fe5
Dave Stanton <davestanton.us@gmail.com>
2024-07-07 12:40:48 -0400

    Including .next to see if sovled Vercel build problem.

--- .next/static/chunks/app/dashboard/customers/page-787299189000d62b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[923,490,281],{501:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[212,938,744],()=>e(501)),_N_E=_.O()}]);
--- .next/static/chunks/app/dashboard/invoices/page-c48a760e7ff45c26.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[490,923,281],{501:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[212,938,744],()=>e(501)),_N_E=_.O()}]);
